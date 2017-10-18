20f60eed592b02a294f850ffd55d484229060cb4
murphy <javalzj@139.com>
2017-10-18 14:07:56 +0800

    hello world

---
+++ crm.service.course/src/main/java/com/rdc/app/crm/service/course/controller/StudentController.java
@@ -0,0 +1,24 @@
+package com.rdc.app.crm.service.course.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rdc.app.crm.service.course.dto.Course;
+import com.rdc.app.crm.service.course.service.StudentService;
+
+
+@RestController
+public class StudentController {
+
+	@Autowired
+	private StudentService studentService;
+
+	@GetMapping("/students/{studentId}/courses")
+	public List<Course> retrieveCoursesForStudent(@PathVariable String studentId) {
+		return studentService.retrieveCourses(studentId);
+	}
+}
